cc9c9d8ccabe2bf72912656198922300c3a64624
Milad <icksa1@gmail.com>
2014-06-26 19:58:25 -0500

    Tweaked flock1.bash

--- flock/flock1.bash
+++ flock/flock1.bash
@@ -5,7 +5,7 @@
 # anyone who attempts to rerun while the sleep is still going will
 # immediately exit. If you run this many times in a row while
 # executing a "ps aux | grep sleep" you will notice that the PID of
-# the sleep process doesn't changeq
+# the sleep process doesn't change
 
 (
     # A process will attempt to aquire an exclusive lock here (-x). If
@@ -14,15 +14,15 @@
     # "exit 1" will be executed - note that this exits from the
     # subprocess (between the parentheses) so the "echo" line at the
     # end will be executed no matter what!
-    flock -x -n 200 || exit 1
+    flock -x -n 200 || { echo "Could not acquire lock"; exit 1; }
+    echo "Lock Acquired"
 
     # If we get here, then the exclusive lock was acquired
-    sleep 60 &
+    sleep 60
 
     # At this point, the lock will be held as long as the sleep
-    # process is running!
-) 200>myfile.lock
-
+    # process is running.
+) 200>myfile.lock &
 
 # This is executed whether or not the lock was acquired, every run of
 # this script should say HELLO!
